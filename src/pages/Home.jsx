61c5986b535ca8018a94adfa1462cb01e722240e
wissem <saadliwissem88@gmail.com>
2023-02-12 00:04:38 +0100

    fixing bugs

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
-import { Navbar } from "../components/navbar/Navbar"
-import Header from "../components/header/Header"
+import { Navbar } from "../components/navbar/Navbar";
+import Header from "../components/header/Header";
 import { Feautured } from "../components/featured/Featured";
 import { PropretyList } from "../components/PropretyList/PropretyList";
 import { FeaturedProperties } from "../components/FeaturedProperties/FeaturedProperties";
@@ -7,18 +7,19 @@ import { MailList } from "../components/MailList/MailList";
 import { Footer } from "../components/foooter/Footer";
 import "./Home.css";
 export const Home = () => {
-    return (<>
-        <Navbar />
-        <Header />
-        <div className="homeContainer"><Feautured />
-            <h1 className="homeTitle">Browse by property type</h1><PropretyList />
-            <h1 className="homeTitle">Homes guests love</h1><FeaturedProperties/>
-            <MailList/>
-            <Footer/>
-            </div>
-            
-
-
+  return (
+    <>
+      <Navbar />
+      <Header />
+      <div className="homeContainer">
+        <Feautured />
+        <h1 className="homeTitle">Browse by property type</h1>
+        <PropretyList />
+        <h1 className="homeTitle">Homes guests love</h1>
+        <FeaturedProperties />
+        <MailList />
+        <Footer />
+      </div>
     </>
-    )
-}
+  );
+};
