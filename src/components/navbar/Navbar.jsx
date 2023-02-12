2b34490eb14d6f549b86de09368ac8a5aaee9f90
wissem <saadliwissem88@gmail.com>
2023-02-12 11:13:45 +0100

    fixing bugs

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,6 +2,7 @@ import React, { useContext } from 'react'
 import "./Navbar.css"
 import {Link} from "react-router-dom"
 import { AuthContext } from '../../context/AuthContext'
+const uri="https://bespoke-treacle-5ca4d6.netlify.app/"
 export const Navbar = () => {
   const { user} = useContext(AuthContext)
 console.log(user)
@@ -14,7 +15,10 @@ console.log(user)
         </Link>
         {user ?  user.details.username :<div className="navItems">
           <button className="navButton">Register</button>
+           <Link to={uri+"/login"}>
+          
           <button className="navButton">Login</button>
+          </Link>
         </div>}
       </div>
     </div>
